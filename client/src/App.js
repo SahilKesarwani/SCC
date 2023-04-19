f30a2afe5df0f93a12de5de2cb082b74accddc60
rahul44895 <83285830+rahul44895@users.noreply.github.com>
2023-04-20 02:39:31 +0530

    Components re-added

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,13 @@
 import React from "react";
-
+import "./style.css"
+import Navbar from "./Components/Navbar";
+import HomePage from "./Components/HomePage";
 const App = () => {
   return (
     <>
-      <div className="navbar">
-        <div className="logoArea"></div>
-        <div className="navOptionsArea">
-          <ul>
-            <li className="navOptions">Home</li>
-            <li className="navOptions">Events</li>
-            <li className="navOptions">Our Team</li>
-            <li className="navOptions">About</li>
-          </ul>
-        </div>
-      </div>
-      <div className="DetailsContainer">
-        <div className="detailsSection">
-          <h1>SCC SIET Coding Club</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
-            velit, ab ad perspiciatis praesentium illo ea ipsam, fugiat fugit
-            quod, explicabo in unde animi corrupti quisquam saepe eligendi
-            facilis hic?
-          </p>
-        </div>
-        <div className="planetContainer">
-          <div className="planet"></div>
-        </div>
-      </div>
-      <div style={{background: "white", width:"100vw", height:"100vh"}}>R</div>
-      <div className="domainsSection">
-        <h1>
-          <center>Domains</center>
-        </h1>
-        <div className="domainsContainer">
-          <div className="domainCardArea">
-            <div className="domainCard">
-              <div className="domainBorderTop"></div>
-              <div className="domainBorderLeft"></div>
-              <div className="domainBorderBottom"></div>
-              <div className="domainBorderRight"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* <div style="height:100vh; background: transparent;"></div> */}
+    <Navbar/>
+    <HomePage/>
+            {/* <div style="height:100vh; background: transparent;"></div> */}
 	  
 
     </>
