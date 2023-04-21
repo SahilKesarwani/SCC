0b5f2ff33dc9dda4efeb41797622fd63c3eec425
rahul44895 <83285830+rahul44895@users.noreply.github.com>
2023-04-21 16:18:41 +0530

    Sample Domain Card Added

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -15,46 +15,63 @@ const HomePage = () => {
           <div className="planet"></div>
         </div>
       </div>
-      <div style={{ width: "100vw", height: "100vh"}}>
-      <div className="carousel">
-            <a href="#one" className="carousel-item">
-                <img
-                    src="./images/oath_to_code.jpg"/>
-                    <h1><center>One Day Challenges</center></h1>
-            </a>
-            <a href="#two" className="carousel-item">
-                <img
-                    src="./images/hackverse.jpg"/>
-                    <h1><center>Hackathons</center></h1>
-            </a>
-            <a href="#three" className="carousel-item">
-                <img
-                    src="./images/bootcamp.jpg"/>
-                    <h1><center>Bootcamp</center></h1>
-            </a>
-            <a href="#four" className="carousel-item">
-                <img
-                    src="./images/30daysChallenge.jpg"/>
-                    <h1><center>30 Days Challenge</center></h1>
-            </a>
-            <a href="#five" className="carousel-item">
-                <img
-                    src="https://www.searchenginejournal.com/wp-content/uploads/2019/07/the-essential-guide-to-using-images-legally-online.png"/>
-                    <h1><center>Join Us</center></h1>
-            </a>
+      <div className="carouselSection">
+        <div className="carousel">
+          <a href="#one" className="carousel-item">
+            <img src="./images/oath_to_code.jpg" />
+            <h1>
+              <center>One Day Challenges</center>
+            </h1>
+          </a>
+          <a href="#two" className="carousel-item">
+            <img src="./images/hackverse.jpg" />
+            <h1>
+              <center>Hackathons</center>
+            </h1>
+          </a>
+          <a href="#three" className="carousel-item">
+            <img src="./images/bootcamp.jpg" />
+            <h1>
+              <center>Bootcamp</center>
+            </h1>
+          </a>
+          <a href="#four" className="carousel-item">
+            <img src="./images/30daysChallenge.jpg" />
+            <h1>
+              <center>30 Days Challenge</center>
+            </h1>
+          </a>
+          <a href="#five" className="carousel-item">
+            <img src="https://www.searchenginejournal.com/wp-content/uploads/2019/07/the-essential-guide-to-using-images-legally-online.png" />
+            <h1>
+              <center>Join Us</center>
+            </h1>
+          </a>
         </div>
       </div>
       <div className="domainsSection">
         <h1>
           <center>Domains</center>
         </h1>
-        <div className="domainsContainer">
-          <div className="domainCardArea">
-            <div className="domainCard">
-              <div className="domainBorderTop"></div>
-              <div className="domainBorderLeft"></div>
-              <div className="domainBorderBottom"></div>
-              <div className="domainBorderRight"></div>
+        <div className="card_container">
+          <div className="parentr">
+            <div className="leftr"></div>
+            <div className="rightr"></div>
+            <div className="topr"></div>
+            <div className="bottomr"></div>
+            <div className="cardr">
+              <img src="https://w0.peakpx.com/wallpaper/469/148/HD-wallpaper-fullstack-development-nodejs-programmer-technology.jpg" />
+                <div className="content">
+                  <h5>Web Development</h5>
+                  <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Excepturi enim doloremque alias dicta ad dolor, numquam eos
+                    itaque quasi rem ut sint.
+                  </p>
+                  <div className="btn">Enroll</div>
+                </div>
+              <div className="overlayr">
+              </div>
             </div>
           </div>
         </div>
