330fe92d3eaf231f0f335662f7a9db0b5fa826b1
Sahil <sahilkesarwani2@gmail.com>
2023-04-29 18:14:14 +0530

    Added Routing

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,19 +1,31 @@
-const Navbar = ({navbar}) => {
-  return (
-    <>
-      <div className="navbar" ref={navbar}>
-        <div className="logoArea"></div>
-        <div className="navOptionsArea">
-          <ul>
-            <li className="navOptions">Home</li>
-            <li className="navOptions">Events</li>
-            <li className="navOptions">Our Team</li>
-            <li className="navOptions">About</li>
-          </ul>
-        </div>
-      </div>
-    </>
-  );
-}
+import { NavLink } from "react-router-dom";
+
+const Navbar = ({ navbar }) => {
+	return (
+		<>
+			<div className="navbar" ref={navbar}>
+				<div className="logoArea">
+					<NavLink to="/" />
+				</div>
+				<div className="navOptionsArea">
+					<ul>
+						<li className="navOptions">
+							<NavLink to="/">Home</NavLink>
+						</li>
+						<li className="navOptions">
+							<NavLink to="/events">Events</NavLink>
+						</li>
+						<li className="navOptions">
+							<NavLink to="/ourteam">Our Team</NavLink>
+						</li>
+						<li className="navOptions">
+							<NavLink to="/about">About</NavLink>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</>
+	);
+};
 
 export default Navbar;
