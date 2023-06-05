d6e172ad619804076471bf1aa9d5be4ae8c17351
rahul44895 <83285830+rahul44895@users.noreply.github.com>
2023-06-06 00:13:40 +0530

    Updated....
    Co-authored-by: Agrahariya <Agrahariya@users.noreply.github.com>
    Co-authored-by: Gauri Gupta <Agrahariya@users.noreply.github.com>

---
+++ client/src/ComponentsNew/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <>
+    <div className="frontSection">
+      SCC - SIET Coding Club
+      <br/>
+      About Page under construction
+    </div>
+    </>
+  )
+}
