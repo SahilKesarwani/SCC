683a4bb51a6d457dcfa941c033dc459b4507bc15
rahul44895 <83285830+rahul44895@users.noreply.github.com>
2023-06-26 02:38:34 +0530

    testing again2
    Co-authored-by: Gauri Gupta <Agrahariya@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -40,7 +40,7 @@ const App = () => {
           path="/ourteam"
           element={<OurTeamPage/>}
         ></Route>
-        <Route exact path="/about" element={<About/>}></Route>
+        {/* <Route exact path="/about" element={<About/>}></Route> */}
         {/* <Route path="*" element={<div style={{color:"black", marginTop:"10%", textAlign:"center", fontSize:"4em"}}>Error 404</div>}></Route> */}
       </Routes>
     </>
