eb091466c3d497c1fe7ee5f6f7a9a57a6c82ca5f
rahul44895 <83285830+rahul44895@users.noreply.github.com>
2023-06-30 18:19:55 +0530

    Updated Event Page recent section
    Co-authored-by: Gauri Gupta <Agrahariya@users.noreply.github.com>

--- client/src/apis/eventsListApi.js
+++ client/src/apis/eventsListApi.js
@@ -4,12 +4,19 @@ const eventsListApi = [
     name: "hackverse1",
     time: "20:00-23:00",
     date: "25 Mar 2023",
+    registrations: "160",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus rem laborum facere fugit possimus ratione nexercitationem vitae enim distinctio officia, vel ullam atque porro assumenda! Voluptatibus ipsa excepturi corporis in voluptate numquam, quo, maiores accusantium voluptas fugit quaerat hic ea voluptatum a commodi amet explicabo! Fuga dolorum quasi dolorem velit magnam aliquam recusandae maxime ducimus facere repudiandae accusamus repellendus fugit aspernatur ipsam suscipit necessitatibus accusantium reprehenderit, explicabo nulla nihil aliquid? Ea ipsum odit maiores, nisi adipisci laboriosam eligendi labore ratione. Modi aspernatur laudantium hic, doloremque itaque quam, corporis exercitationem accusamus assumenda reprehenderit pariatur quasi odio sed id aperiam deserunt asperiores!",
-    imageURL: [{ imgURL: "img1URL.com" }, { imgURL: "img2URL.com" }],
+    imageURL: [
+      {
+        imgURL:
+          "https://wallpapers.com/images/featured/4k-space-9w27dqmc4nrs3xwd.jpg",
+      },
+      { imgURL: "https://images4.alphacoders.com/118/thumb-1920-1182562.png" },
+    ],
     domainOfEvent: "domains",
     totalParticipants: "200+",
-    sponsors: "CodingNinjas",
+    sponsors: "Coding Ninjas",
     winners: [
       {
         teamName: "Midnight Coders",
